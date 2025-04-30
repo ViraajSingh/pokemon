c4e49ca080c8ab3374b010ca447080b713f17985
ViraajSingh <write2viraaj@gmail.com>
2025-04-29 19:51:54 -0700

    does

--- pokehomework/script2.js
+++ pokehomework/script2.js
@@ -1,6 +1,52 @@
-$(document).ready(function () {//makig sure that the documet is loaded before we do aything 
+$(document).ready(function () {
+    function clearOutput() {//a way to make sure that there is othign in the output div in the html when we call this fuctio 
+        document.getElementById("output").innerHTML = "";
+    }
 
-    const favoritePokemon = [//giving an array of the 10 pokemoen so that it is not just the first 10 anymore 
+    // this i s how we woudl get the ccertai data by usig the fetch with the certia amme of tthe poke o i order to do this 
+    function Cards(data) {
+        const output = document.getElementById("output");
+        output.innerHTML += `
+            <div class="card">
+                <h2>${data.name.charAt(0).toUpperCase() + data.name.slice(1)}</h2>
+                <img src="${data.sprites.front_default}" alt="${data.name}">
+                <p>Abilities: ${data.abilities[0].ability.name}</p>
+                <p>Types: ${data.types[0].type.name}</p>
+                <p>Base Experience: ${data.base_experience}</p>
+            </div>`;
+    }
+
+    //how we do the seach function 
+    $("#search-button").click(function () {
+        const query = document.getElementById("search-input").value.toLowerCase();//makign sure that name of the pokem which we get is lowwercase sice we imbed it into the url for the apu 
+        if (!query) return;//numbers or empty string will not work
+
+        clearOutput();//make sure that after we store the name we clear it 
+
+        fetch(`https://pokeapi.co/api/v2/pokemon/${query}`)//fid the data of the certia ame of the pokemo that we just got 
+            .then(response => {
+                if (!response.ok) {
+                    throw new Error("Pokémon not found.");//this is if the repose messed up then we knwo they put in smt that was not a pokemon 
+                }
+                return response.json();//makign sure to retunr the repionse in case 
+            })
+            .then(pokemonData => {
+                Cards(pokemonData);//getting the data of that certian pokemon 
+            })
+            .catch(error => {
+                $("#output").html(`<p">${error.message}</p>`);//displayign the messsage if we do go wrog 
+            });
+    });
+
+    // Enter key triggers search
+    $("#search-input").on("keypress", function (e) {//this is dong the same function as the click but with the key down o the enter key 
+        if (e.which === 13) {
+            $("#search-button").click();
+        }
+    });
+
+    // this will display a certian 1 istead oif the first 10 i the other page 
+    const favoritePokemon = [
         "gengar",
         "charizard",
         "bulbasaur",
@@ -13,37 +59,25 @@ $(document).ready(function () {//makig sure that the documet is loaded before we
         "scizor"
     ];
 
-    function Cards(data) {//same function to produce the cards as before siunce othign here changes 
-        const output = document.getElementById("output");
-        output.innerHTML += `
-            <div class="card">
-                <h2>${data.name}</h2>
-                <img src="${data.sprites.front_default}" alt="${data.name}">
-                <p>Abilities: ${data.abilities[0].ability.name}</p>
-                <p>Types: ${data.types[0].type.name}</p>
-                <p>Base Experience: ${data.base_experience}</p>
-            </div>`;
-    }
+    const display = document.getElementById("poke-list");//display the ames of the pokemon 
 
-    const display = document.getElementById("poke-list");//this is where just the names of the pokemon will be displayed
-
-    favoritePokemon.forEach(name => {//for every have we woudl make tghe li and then add the name to that li then add that li to the cost display 
+    favoritePokemon.forEach(name => {//this is how we owudl get the name then put it to an li and then from tehr we woudl put the li in the pooutput 
         const li = document.createElement("li");
         li.textContent = name;
         display.appendChild(li);
 
-        fetch(`https://pokeapi.co/api/v2/pokemon/${name}`)//here we are mking the fetch call to get the data for the pokemon and we woudl chnage it for every ame i the array name that we made before allowing us to go through each of the epokemen and get its sdata 
+        fetch(`https://pokeapi.co/api/v2/pokemon/${name}`)//doing the cards for the na,ees in the favorite pokemon 
             .then(response => {
                 if (!response.ok) {
-                    throw new Error('Network response was not ok ' + response.statusText);//makign sure that the fetch was okay 
+                    throw new Error('Network response was not ok ' + response.statusText);
                 }
-                return response.json();//return if the fetch was ntoto okay 
+                return response.json();
             })
             .then(pokemonData => {
-                Cards(pokemonData);//makign thee fucntion with that specific pokemon that we are curretly allog as we go through the bnames 
+                Cards(pokemonData);//this will just get the data for that pokemon in the array 
             })
             .catch(error => {
-                console.error('Error fetching pokemon details:', error);//to make sure there re any errr at all 
+                console.error('Error fetching Pokémon details:', error);
             });
     });
 });
